1947f0709a667d629486b54820d6a9ffe13b3093
Justas Vitaitis <justas@u3.lt>
2021-05-01 20:09:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# concurrent sharded map
+
+concurrent safe hash map. If you don't need shards please check `sync.Map`
+
+## Original author
+
+https://www.openmymind.net/Shard-Your-Hash-table-to-reduce-write-locks/
---
+++ concurrent_sharded_map.go
@@ -0,0 +1,53 @@
+package concurrent_sharded_map
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"sync"
+)
+
+type ConcurrentShardedMap map[string]*Shard
+
+type Shard struct {
+	items map[string]interface{}
+	lock *sync.RWMutex
+}
+
+func New() ConcurrentShardedMap {
+	c := make(ConcurrentShardedMap, 256)
+
+	for i := 0; i < 256; i++ {
+		c[fmt.Sprintf("%02x", i)] = &Shard{
+			items: make(map[string]interface{}, 2048),
+			lock: new(sync.RWMutex),
+		}
+	}
+
+	return c
+}
+
+func (c ConcurrentShardedMap) Get(key string) interface{} {
+	shard := c.getShard(key)
+	shard.lock.RLock()
+
+	defer shard.lock.RUnlock()
+
+	return shard.items[key]
+}
+
+func (c ConcurrentShardedMap) Set(key string, data interface{}) {
+	shard := c.getShard(key)
+	shard.lock.Lock()
+
+	defer shard.lock.Unlock()
+
+	shard.items[key] = data
+}
+
+func (c ConcurrentShardedMap) getShard(key string) (shard *Shard) {
+	hasher := sha1.New()
+	hasher.Write([]byte(key))
+	shardKey :=  fmt.Sprintf("%x", hasher.Sum(nil))[0:2]
+
+	return c[shardKey]
+}
